d619bdf7afecd63034edd8ab691c0d9ab3eabd23
Luciano De Gianni <lucdegianni@gmail.com>
2026-01-31 10:26:01 -0800

    chore: complete available filters

--- dt/lib/filter.py
+++ dt/lib/filter.py
@@ -3,17 +3,22 @@ from abc import ABC, abstractmethod
 from enum import Enum
 
 class Filter(ABC):
+    @classmethod
+    @abstractmethod
+    def from_dict(cls, f_dict: dict) -> Filter:
+        pass
+
     @abstractmethod
     def apply(self, df: pd.DataFrame) -> pd.Series:
-        """Apply the given transformation"""
         pass
     
     @abstractmethod
     def __str__(self) -> str:
         pass
 
-    def __repr__(self) -> str:
-        return self.__str__()
+class FilterError(ValueError):
+    def __init__(self, filter: str, message: str):
+        super().__init__(f"{filter} filter error {message}")
 
 class NotNull(Filter):
     """Filter for checking that a column is not null"""
@@ -21,47 +26,116 @@ class NotNull(Filter):
         super().__init__()
         self.column = column
 
+    @classmethod
+    def from_dict(cls, f_dict: dict) -> NotNull:
+        column = f_dict.get("column")
+        if isinstance(column, (int, str)):
+            return cls(column)
+        else:
+            raise FilterError("NotNull", f"column {column} is not a valid string or integer")
+
     def apply(self, df: pd.DataFrame) -> pd.Series:
         return df[self.column].notnull()
     
     def __str__(self) -> str:
-        return f"NotNull(column={self.column})"
+        return f"{self.__class__.__name__}(column={self.column})"
+    
 
-class GreaterThan(Filter):
-    """Filter for checking if a value is greater than another value"""
+class Regex(Filter):
+    """Filter for checking that a column matches a regex pattern"""
+    def __init__(self, column: str | int, pattern: str):
+        super().__init__()
+        self.column = column
+        self.pattern = pattern
+
+    @classmethod
+    def from_dict(cls, f_dict: dict) -> Regex:
+        column = f_dict.get("column")
+        pattern = f_dict.get("pattern")
+        if isinstance(column, (int, str)) and isinstance(pattern, str):
+            return cls(column, pattern)
+        else:
+            raise FilterError("Regex", f"column {column} or pattern {pattern} are not valid types")
+
+    def apply(self, df: pd.DataFrame) -> pd.Series:
+        return df[self.column].astype(str).str.match(self.pattern)
+    
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(column={self.column}, pattern={self.pattern})"
+
+
+class ComparisonFilter(Filter):
     def __init__(self, column: str | int, threshold: int | float):
         super().__init__()
         self.column = column
         self.threshold = threshold
 
+    @classmethod
+    def from_dict(cls,f_dict: dict) -> ComparisonFilter:
+        column = f_dict.get("column")
+        threshold = f_dict.get("threshold")
+        if isinstance(column, (int, str)) and isinstance(threshold, (int, float)):
+            return cls(column, threshold)
+        else:
+            raise FilterError(cls.__name__, f"column {column} or threshold {threshold} are not valid types")
+    
+    @abstractmethod
+    def apply(self, df: pd.DataFrame) -> pd.Series:
+        pass
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(column={self.column}, threshold={self.threshold})"
+
+class GreaterThan(ComparisonFilter):
     def apply(self, df: pd.DataFrame) -> pd.Series:
         return df[self.column] > self.threshold
     
-    def __str__(self) -> str:
-        return f"GreaterThan(column={self.column}, threshold={self.threshold})"
+class GreaterThanEqualTo(ComparisonFilter):
+    def apply(self, df: pd.DataFrame) -> pd.Series:
+        return df[self.column] >= self.threshold
+
+class EqualTo(ComparisonFilter):
+    def apply(self, df: pd.DataFrame) -> pd.Series:
+        return df[self.column] == self.threshold
+
+class LessThan(ComparisonFilter):
+    def apply(self, df: pd.DataFrame) -> pd.Series:
+        return df[self.column] < self.threshold
+
+class LessThanEqualTo(ComparisonFilter):
+    def apply(self, df: pd.DataFrame) -> pd.Series:
+        return df[self.column] <= self.threshold
 
 class FilterType(Enum):
+    """Used in FilterFactory to identify filter types"""
     NOT_NULL = "not_null"
     GREATER_THAN = "greater_than"
+    EQUAL_TO = "equal_to"
+    LESS_THAN = "less_than"
+    GREATER_THAN_EQUAL_TO = "greater_than_equal_to"
+    LESS_THAN_EQUAL_TO = "less_than_equal_to"
+    REGEX = "regex"
 
 class FilterFactory:
+    """Factory for creating Filter objects from dictionaries"""
     @staticmethod
     def from_dict(f_dict: dict) -> Filter:
+        """Create a Filter object from a dictionary"""
         f_type = FilterType(f_dict.get("type"))
         match f_type:
             case FilterType.NOT_NULL:
-                column = f_dict.get("column")
-                if isinstance(column, (int, str)):
-                    return NotNull(column)
-                else:
-                    raise ValueError("NotNull filter requires a string or integer column identifier.")
+                return NotNull.from_dict(f_dict)
+            case FilterType.REGEX:
+                return Regex.from_dict(f_dict)
             case FilterType.GREATER_THAN:
-                column = f_dict.get("column")
-                threshold = f_dict.get("threshold")
-
-                if isinstance(column, (int, str)) and isinstance(threshold, (int, float)):
-                    return GreaterThan(column, threshold)
-                else:
-                    raise ValueError("GreaterThan filter requires a string or integer column identifier and a numeric threshold.")
+                return GreaterThan.from_dict(f_dict)
+            case FilterType.GREATER_THAN_EQUAL_TO:
+                return GreaterThanEqualTo.from_dict(f_dict)
+            case FilterType.EQUAL_TO:
+                return EqualTo.from_dict(f_dict)
+            case FilterType.LESS_THAN:
+                return LessThan.from_dict(f_dict)
+            case FilterType.LESS_THAN_EQUAL_TO:
+                return LessThanEqualTo.from_dict(f_dict)
             case _:
                 raise ValueError(f"Filter type not supported: {f_type}")
--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@ import unittest
 import pandas as pd
 import dt.lib.transform as transform
 import dt.io.reader as r
+import dt.lib.filter as filter_lib
 
 class TestTransforms(unittest.TestCase):
     def test_TrimStrings(self):
@@ -32,6 +33,150 @@ class TestTransforms(unittest.TestCase):
         t_df = t.transform(df)
 
         self.assertTrue(t_df.equals(expected_df))
+
+class TestFilters(unittest.TestCase):
+    def test_from_dict(self):
+        not_null_dict = {
+            "type": "not_null",
+            "column": "age"
+        }
+
+        regex_dict = {
+            "type": "regex",
+            "column": "code",
+            "pattern": r"^[A-Z]{3}\d{3}$"
+        }
+
+        greater_than_dict = {
+            "type": "greater_than",
+            "column": "value",
+            "threshold": 10
+        }
+
+        greater_than_equal_to_dict = {
+            "type": "greater_than_equal_to",
+            "column": "value",
+            "threshold": 10
+        }
+
+        equal_to_dict = {
+            "type": "equal_to",
+            "column": "value",
+            "threshold": 10
+        }
+
+        less_than_dict = {
+            "type": "less_than",
+            "column": "value",
+            "threshold": 10
+        }
+
+        less_than_equal_to_dict = {
+            "type": "less_than_equal_to",
+            "column": "value",
+            "threshold": 10
+        }
+
+        not_null_filter = filter_lib.FilterFactory.from_dict(not_null_dict)
+        regex_filter = filter_lib.FilterFactory.from_dict(regex_dict)
+        greater_than_filter = filter_lib.FilterFactory.from_dict(greater_than_dict)
+        greater_than_equal_to_filter = filter_lib.FilterFactory.from_dict(greater_than_equal_to_dict)
+        equal_to_filter = filter_lib.FilterFactory.from_dict(equal_to_dict)
+        less_than_filter = filter_lib.FilterFactory.from_dict(less_than_dict)
+        less_than_equal_to_filter = filter_lib.FilterFactory.from_dict(less_than_equal_to_dict)
+
+        self.assertIsInstance(not_null_filter, filter_lib.NotNull)
+        self.assertIsInstance(regex_filter, filter_lib.Regex)
+        self.assertIsInstance(greater_than_filter, filter_lib.GreaterThan)
+        self.assertIsInstance(greater_than_equal_to_filter, filter_lib.GreaterThanEqualTo)
+        self.assertIsInstance(equal_to_filter, filter_lib.EqualTo)
+        self.assertIsInstance(less_than_filter, filter_lib.LessThan)
+        self.assertIsInstance(less_than_equal_to_filter, filter_lib.LessThanEqualTo)
+
+    def test_NotNull(self):
+        df = pd.DataFrame({
+            "one": [1, None, 3],
+            "two": [1, 2, 3]
+        })
+
+        filter = filter_lib.NotNull("one")
+
+        filteredSeries = filter.apply(df)
+        expectedSeries = pd.Series([True, False, True])
+
+        self.assertTrue(filteredSeries.equals(expectedSeries))
+
+    def test_Regex(self):
+        df = pd.DataFrame({
+            "codes": ["ABC123", "DEF456", "hello", "JKL012"]
+        })
+
+        filter = filter_lib.Regex("codes", r"^[A-Z]{3}\d{3}$")
+
+        filteredSeries = filter.apply(df)
+        expectedSeries = pd.Series([True, True, False, True])
+
+        self.assertTrue(filteredSeries.equals(expectedSeries))
+
+    def test_GreaterThan(self):
+        df = pd.DataFrame({
+            "values": [10, 20, 30, 40, 50]
+        })
+
+        filter = filter_lib.GreaterThan("values", 25)
+
+        filteredSeries = filter.apply(df)
+        expectedSeries = pd.Series([False, False, True, True, True])
+
+        self.assertTrue(filteredSeries.equals(expectedSeries))
+
+    def test_GreaterThanEqualTo(self):
+        df = pd.DataFrame({
+            "values": [10, 20, 30, 40, 50]
+        })
+
+        filter = filter_lib.GreaterThanEqualTo("values", 30)
+
+        filteredSeries = filter.apply(df)
+        expectedSeries = pd.Series([False, False, True, True, True])
+
+        self.assertTrue(filteredSeries.equals(expectedSeries))
+
+    def test_EqualTo(self):
+        df = pd.DataFrame({
+            "values": [10, 20, 30, 40, 50]
+        })
+
+        filter = filter_lib.EqualTo("values", 30)
+
+        filteredSeries = filter.apply(df)
+        expectedSeries = pd.Series([False, False, True, False, False])
+
+        self.assertTrue(filteredSeries.equals(expectedSeries))
+
+    def test_LessThan(self):
+        df = pd.DataFrame({
+            "values": [10, 20, 30, 40, 50]
+        })
+
+        filter = filter_lib.LessThan("values", 30)
+
+        filteredSeries = filter.apply(df)
+        expectedSeries = pd.Series([True, True, False, False, False])
+
+        self.assertTrue(filteredSeries.equals(expectedSeries))
+
+    def test_lessThanEqualTo(self):
+        df = pd.DataFrame({
+            "values": [10, 20, 30, 40, 50]
+        })
+
+        filter = filter_lib.LessThanEqualTo("values", 30)
+
+        filteredSeries = filter.apply(df)
+        expectedSeries = pd.Series([True, True, True, False, False])
+
+        self.assertTrue(filteredSeries.equals(expectedSeries))
 
 class TestCSVReader(unittest.TestCase):
     def test_ReadSingleCSV(self):
